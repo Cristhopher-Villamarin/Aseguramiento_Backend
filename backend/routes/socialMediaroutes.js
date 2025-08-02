a801a4db993e29d2cb8a4083d664a286ddf96854
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2025-08-02 12:24:17 -0500

    update conexión con clasificador de comentarios

--- backend/routes/socialMediaroutes.js
+++ backend/routes/socialMediaroutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getSocialMediaComments } = require('../controllers/socialMediaController');
+const { getSocialMediaComments, analyzeSentiment} = require('../controllers/socialMediaController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Route to get social media comments
 router.post('/comments', authMiddleware, getSocialMediaComments);
+
+// Route to analyze sentiment
+router.post('/analyze-sentiment', authMiddleware, analyzeSentiment);
 
 module.exports = router;
